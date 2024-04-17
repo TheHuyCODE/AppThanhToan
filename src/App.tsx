c81d001c29e44856a506851b1aa63d3319307269
nguyenthehuynosleep <nguyenthehuy202th@gmail.com>
2024-04-17 10:44:11 +0700

    17/04

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import LoginRegister from "./components/Register/LoginRegister";
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LoginRegister/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Register/LoginRegister.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./LoginRegister.css";
+import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
+
+const LoginRegister = () => {
+  const [action, setAction] = useState("");
+
+  const registerLink = () => {
+    setAction(" active");
+  };
+  const loginLink = () => {
+    setAction("");
+  };
+  return (
+    <div className={`wrapper${action}`}>
+      {/* form login */}
+      <div className="form-box login">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label htmlFor="">
+              <input type="checkbox"></input>
+              Remember me
+            </label>
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>
+              Don't have an account?{" "}
+              <a href="#" onClick={registerLink}>
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+
+      {/* form register */}
+      <div className="form-box register">
+        <form action="">
+          <h1>Registration</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder="Email" required />
+            <FaEnvelope className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label htmlFor="">
+              <input type="checkbox"></input>I agree to the terms and conditions
+            </label>
+          </div>
+          <button type="submit">Register</button>
+          <div className="register-link">
+            <p>
+              Don't have an account?{" "}
+              <a href="#" onClick={loginLink}>
+                Login
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginRegister;
