4e8536b52a9cc7548d1b11d962d0e854194c4ad4
nguyenthehuynosleep <nguyenthehuy202th@gmail.com>
2024-04-17 12:16:05 +0700

    done-login

--- src/components/Register/LoginRegister.tsx
+++ src/components/Register/LoginRegister.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import "./LoginRegister.css";
-import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
-
+import {
+  FaUser,
+  FaLock,
+  FaEnvelope,
+  FaPhone,
+  FaLockOpen,
+} from "react-icons/fa";
+import { IoMdHome } from "react-icons/io";
 const LoginRegister = () => {
   const [action, setAction] = useState("");
 
@@ -11,6 +17,14 @@ const LoginRegister = () => {
   const loginLink = () => {
     setAction("");
   };
+
+  const [codeStore, setCodestore] = useState("");
+  const [userName, setUsername] = useState("");
+  const [passWord, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const hiddenPassword = () => {
+    console.log("hidden password");
+  };
   return (
     <div className={`wrapper${action}`}>
       {/* form login */}
@@ -18,12 +32,50 @@ const LoginRegister = () => {
         <form action="">
           <h1>Login</h1>
           <div className="input-box">
-            <input type="text" placeholder="Username" required />
+            <input
+              type="text"
+              value={codeStore}
+              placeholder="Mã gian hàng"
+              required
+              onChange={(event) => {
+                setCodestore(event.target.value);
+              }}
+            />
+            <IoMdHome className="icon home" />
+          </div>
+          <div className="input-box">
+            <input
+              type="text"
+              value={userName}
+              placeholder="Username"
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              required
+            />
             <FaUser className="icon" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required />
-            <FaLock className="icon" />
+            <input
+              type={showPassword === true ? "text" : "password"}
+              value={passWord}
+              placeholder="Password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              required
+            />
+            {showPassword ? (
+              <FaLockOpen
+                className="icon password"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            ) : (
+              <FaLock
+                className="icon password"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            )}
           </div>
           <div className="remember-forgot">
             <label htmlFor="">
@@ -32,7 +84,13 @@ const LoginRegister = () => {
             </label>
             <a href="#">Forgot Password?</a>
           </div>
-          <button type="submit">Login</button>
+          <button
+            type="submit"
+            className={codeStore && userName && passWord ? "active" : ""}
+            disabled={codeStore && userName && passWord ? false : true}
+          >
+            Login
+          </button>
           <div className="register-link">
             <p>
               Don't have an account?{" "}
@@ -53,11 +111,19 @@ const LoginRegister = () => {
             <FaUser className="icon" />
           </div>
           <div className="input-box">
+            <input type="text" placeholder="Phone number" required />
+            <FaPhone className="icon" />
+          </div>
+          <div className="input-box">
             <input type="text" placeholder="Email" required />
             <FaEnvelope className="icon" />
           </div>
           <div className="input-box">
             <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Comfirm Password" required />
             <FaLock className="icon" />
           </div>
           <div className="remember-forgot">
@@ -68,7 +134,7 @@ const LoginRegister = () => {
           <button type="submit">Register</button>
           <div className="register-link">
             <p>
-              Don't have an account?{" "}
+              Return to page Login?{" "}
               <a href="#" onClick={loginLink}>
                 Login
               </a>
